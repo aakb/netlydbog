56976e8260af26e9f33de5c54f63be1a054aeabe
Jesper Kristensen <cableman@linuxdev.dk>
2012-06-14 11:29:36 +0200

    Removed unused ajax modules

--- sites/all/modules/ajaxify_regions/js/ajaxify_regions.js
+++
@@ -1,7 +0,0 @@
-function ajaxify_regions_success_block(data)
-{
-  for(key in data) {
-     var block = $('#block-'+key+'-ajax-content').replaceWith(data[key]['content']);
-     Drupal.attachBehaviors(block);
-  }
-}
