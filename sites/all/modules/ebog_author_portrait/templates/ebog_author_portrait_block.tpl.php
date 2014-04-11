ae68d9c5c29573a938cb99a6d4f399e42debcffc
Jesper Kristensen <cableman@linuxdev.dk>
2014-04-11 22:44:07 +0200

    Updated ebog author portrait module to use new portrait content type

--- sites/all/modules/ebog_author_portrait/templates/ebog_author_portrait_block.tpl.php
+++ sites/all/modules/ebog_author_portrait/templates/ebog_author_portrait_block.tpl.php
@@ -6,15 +6,15 @@
 ?>
 <div class="ebog-author-portrait">
   <div class="ebog-author-portrait-image">
-    <?php echo theme_image($conf['image'], '' , '', array(), FALSE); ?>
+    <?php echo theme_image($image, '' , '', array(), FALSE); ?>
   </div>
   <div class="ebog-author-portrait-descr">
-    <h3 class="title"><?php echo $conf['portrait']['name']; ?></h3>
-    <p class="teaser"><?php echo $conf['portrait']['teaser']; ?></p>
+    <h3 class="title"><?php echo $name; ?></h3>
+    <p class="teaser"><?php echo $description; ?></p>
     <p class="forfatterweb-link"><?php echo l(theme_image(drupal_get_path('theme', 'ebog') . '/images/forfatterweb.jpg', '' , '', array(), FALSE), 'http://www.forfatterweb.dk/', array('html' => TRUE, 'absolute' => TRUE, 'attributes' => array('target' => '_blank', 'title' => 'Forfatterweb'))); ?></p>
   </div>
   <div class="clear"></div>
 </div>
 <div class="bottom-bar">
-  <div class="see-more"><?php echo l($conf['portrait']['url'], $conf['portrait']['more'], array('attributes' => array('target' => '_blank', 'title' => $conf['portrait']['more']))); ?></div>
+  <div class="see-more"><?php echo $more_link; ?></div>
 </div>
