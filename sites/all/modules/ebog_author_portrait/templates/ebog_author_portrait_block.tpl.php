aec3518cffaf9848fc9729d3f4d5e1a9cef2da67
Jesper Kristensen <cableman@linuxdev.dk>
2014-04-12 22:49:08 +0200

    Clean up code in ebog author portrait module

--- sites/all/modules/ebog_author_portrait/templates/ebog_author_portrait_block.tpl.php
+++ sites/all/modules/ebog_author_portrait/templates/ebog_author_portrait_block.tpl.php
@@ -6,12 +6,12 @@
 ?>
 <div class="ebog-author-portrait">
   <div class="ebog-author-portrait-image">
-    <?php echo theme_image($image, '' , '', array(), FALSE); ?>
+    <?php echo theme_image($image, '', '', array(), FALSE); ?>
   </div>
   <div class="ebog-author-portrait-descr">
     <h3 class="title"><?php echo $name; ?></h3>
     <p class="teaser"><?php echo $description; ?></p>
-    <p class="forfatterweb-link"><?php echo l(theme_image(drupal_get_path('theme', 'ebog') . '/images/forfatterweb.jpg', '' , '', array(), FALSE), 'http://www.forfatterweb.dk/', array('html' => TRUE, 'absolute' => TRUE, 'attributes' => array('target' => '_blank', 'title' => 'Forfatterweb'))); ?></p>
+    <p class="forfatterweb-link"><?php echo l(theme_image(drupal_get_path('theme', 'ebog') . '/images/forfatterweb.jpg', '', '', array(), FALSE), 'http://www.forfatterweb.dk/', array('html' => TRUE, 'absolute' => TRUE, 'attributes' => array('target' => '_blank', 'title' => 'Forfatterweb'))); ?></p>
   </div>
   <div class="clear"></div>
 </div>
