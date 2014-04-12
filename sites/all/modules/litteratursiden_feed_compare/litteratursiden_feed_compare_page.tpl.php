eca861fe66f26f91d3c29c1596b2378f2566f8b1
Jesper Kristensen <cableman@linuxdev.dk>
2014-04-12 22:24:26 +0200

    Updated litteratursiden feed compare to match eroelen

--- sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_page.tpl.php
+++ sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_page.tpl.php
@@ -4,20 +4,20 @@
  */
 ?>
 <div class="feed-and-compare-page clear-block">
-  <?php if ($items['status'] == 'empty') { ?>
+  <?php if ($items['status'] == 'empty') : ?>
     <div class="feed_and_compare_empty">
       <h1>Der var desv&aelig;rre ingen materialer som matchede anmeldelser hos Litteratursiden.</h1>
     </div>
-  <?php } ?>
+  <?php endif ?>
 
-  <?php if ($items['status'] == 'error') { ?>
+  <?php if ($items['status'] == 'error') : ?>
     <div class="feed_and_compare_error">
       <b>Error:</b> <?php echo $items['message']; ?>
     </div>
-  <?php } ?>
+  <?php endif; ?>
 
-  <?php if (($items['status'] == 'ok') || ($items['status'] == 'notfull')) { ?>
-    <?php foreach ($items['data'] as $key => $item) { ?>
+  <?php if (($items['status'] == 'ok') || ($items['status'] == 'notfull')) : ?>
+    <?php foreach ($items['data'] as $key => $item) : ?>
       <div class="feed_and_compare_item display-book">
         <div class="left">
         <?php
@@ -28,17 +28,16 @@
         </div>
         <div class="record right">
           <h3 class="title">
-            <?php echo l($item['title'], $item['url'], array('html' => true)); ?>
+            <?php echo l($item['title'], $item['url'], array('html' => TRUE)); ?>
           </h3>
           <div class="author">
-            <?php echo l($item['author'], 'ting/search/' . urlencode($item['author']), array('html' => true)); ?>
+            <?php echo l($item['author'], 'ting/search/' . urlencode($item['author']), array('html' => TRUE)); ?>
           </div>
           <div class="descr">
             <?php echo $item['abstract']; ?>
           </div>
         </div>
       </div>
-    <?php } ?>
-  <?php } ?>
-
+    <?php endforeach; ?>
+  <?php endif; ?>
 </div>
